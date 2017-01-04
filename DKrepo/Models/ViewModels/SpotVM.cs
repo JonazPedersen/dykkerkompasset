2c8ebf0ee3dfa55623565714b2696b557d78b464
mads422g <mads422g@VID69PFH12>
2017-01-04 10:03:19 +0100

    SpotVM

---
+++ DKrepo/Models/ViewModels/SpotVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DKrepo
+{
+    public class SpotVM
+    {
+        public Spot spot { get; set; }
+
+        public List<Billeder> billeder { get; set; }
+    }
+}
